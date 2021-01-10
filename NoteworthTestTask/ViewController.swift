6a722faadf8cb3ce791cbbb930791bd80dfa13bb
Dmytro Skorokhod <dima.skorokhod@gmail.com>
2021-01-10 19:35:31 +0200

    Initial Commit

---
+++ NoteworthTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteworthTestTask
+//
+//  Created by  Admin on 10.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
